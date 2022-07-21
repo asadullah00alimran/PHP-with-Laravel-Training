849c2660e2346e93e1a3d99ae1b4c3363f29f3ea
Asadullah Al Imran <asadullahalimran00@gmail.com>
2022-07-21 15:55:46 +0600

    Add files via upload

---
+++ Day 24/index.php
@@ -0,0 +1,45 @@
+<?php
+ if(isset)
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="" method="post">
+    <table border="1">
+        <tr>
+            <th>SL No.</th>
+            <th>Sub Code</th>
+            <th>Sub Name</th>
+            <th>Practical Mark</th>
+            <th>Theoty Marks</th>
+            <th>Total</th>
+            <th>Grade</th>
+            <th>Point</th>
+            <th>Button</th>
+        </tr>
+        <tr>
+            <td>01</td>
+            <td>1009</td>
+            <td>Physics</td>
+            <td><input type="text" name="pm"></td>
+            <td><input type="text" name="thm"></td>
+            <td><input type="text" name="ttm"></td>
+            <td><input type="text" name="gr"></td>
+            <td><input type="text" name="pnt"></td>
+            <td><input type="button" value="Get Result"></td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
